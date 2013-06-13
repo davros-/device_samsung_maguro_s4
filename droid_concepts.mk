3401e63c66dbb9321d8ba9fb0c655eb02aa223c2
davros <davros@droidconcepts.com>
2013-06-13 02:32:47 -0400

    fixing maguro

--- droid_concepts.mk
+++ droid_concepts.mk
@@ -5,24 +5,24 @@ PRODUCT_RELEASE_NAME := GN-GSM
 OVERLAY_TARGET := pa_xhdpi
 
 # gsm
-$(call inherit-product, vendor/DEMENTED/config/common_gsm.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_gsm.mk)
 
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
 # phone
-$(call inherit-product, vendor/DEMENTED/config/common_phone.mk)
+$(call inherit-product, vendor/Droid_Concept/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := demented_maguro
+PRODUCT_NAME := droid_concepts_maguro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=DEMENTED.maguro.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=Droid_Concepts.maguro.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
